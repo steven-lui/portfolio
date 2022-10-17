638d3ba15b7d599b2a4cf3d59d2251916286ec6d
steven-lui <steven.lui@netmatters-scs.com>
2022-10-17 16:59:41 +0100

    Update fade-in.js
    Fixed the fade in/out methods

--- js/fade-in.js
+++ js/fade-in.js
@@ -1,15 +1,13 @@
 /**
- * Run the check if each section scrolls into the threshold
- * Displays the section if it passes the checks
- * @param {integer} scroll_dir 
+ * Update each section/article if it is within the threshold
+ * Show if it is, hide otherwise
  */
-function update_sections(scroll_dir) {
-    // Iterate through each section in the main
-    $('main section').each(function () {
+function update_sections() {
+    // Iterate through each section OR articles (see examples.html) in the main
+    $('main section, main section article').each(function () {
         // top and bottom of section
         var top = $(this).position().top;
         var bot = $(this).position().top + $(this).outerHeight();
-
         // thresholds are a smaller section of the window height to check if either ends of the section is in view
         // the middle third is the threshold
         var window_third = $(window).height() / 3;
@@ -17,77 +15,26 @@ function update_sections(scroll_dir) {
         var bot_threshold = $(window).scrollTop() + (2 * window_third);
 
         /**
-         * show on scroll down: top enters bot_threshold
-         * hide on scroll down: bottom leaves top_threshold
-         * show on scroll up: bottom enters top_threshold
-         * hide on scroll up: top leaves bot_threshold
-         * show on start: if section is on page
+         * Check if all of the item is not in the threshold
          */
-        switch (scroll_dir) {
-            // scroll down
-            case -1:
-                // show
-                if (top > bot_threshold) display_section(this, true);
-                // hide
-                // else if (bot < top_threshold) display_section(this, false);
-                break;
-            // scroll up
-            case 1:
-                // show
-                if (bot < top_threshold) display_section(this, true);
-                // hide
-                // else if (top > bot_threshold) display_section(this, false);
-                break;
-            // page start
-            default:
-                display_section(this, top < $(window).height());
-                break;
-        }
+        ((top > bot_threshold && bot > bot_threshold) || (top < top_threshold && bot < top_threshold)) ?
+            $(this).stop(true).animate({ 'opacity': '0' }, 500) : // show if true
+            $(this).stop(true).animate({ 'opacity': '1' }, 500); // hide if false
     });
-}
-
-/**
- * Show/hide section on page
- * @param {object} section 
- * @param {boolean} show 
- */
-function display_section(section, show) {
-    show ?
-        $(section).animate({ 'opacity': '1' }, 1500) : // show if true
-        $(section).animate({ 'opacity': '0' }, 1500); // hide if false
 }
 
 // https://codepen.io/annalarson/pen/AegVzq
 // page ready
 $(document).ready(function () {
     // set position to top
-    $(window)
-        .scrollTop(0)
-        .promise()
-        .done(function () {
-            // one time check to make sections visible on page load
-            update_sections(0);
-        }
-        );
+    $(window).scrollTop(0);
 
-    // last recorded position on the page
-    var lastPos = 0;
-    // scroll direction, up is positive
-    var scroll_dir = 0;
+    // run at start of page once
+    update_sections();
+});
 
-    // on page scroll
-    $(window).scroll(function () {
-        // new position on the page
-        var currentPos = $(window).scrollTop();
-
-        // scroll direction, scroll is positive
-        scroll_dir = currentPos > lastPos ? -1 : 1;
-        console.log(scroll_dir);
-
-        // update position on page
-        lastPos = currentPos;
-
-        // run check on sections, now we know the scroll direction
-        update_sections(scroll_dir);
-    });
+// on page scroll
+$(window).scroll(function () {
+    // run check on sections, now we know the scroll direction
+    update_sections();
 });
